162c2625e5afa67c8d6006f738d8c64b3c659232
Anderson Salles <agsalles@gmail.com>
2017-08-04 10:17:11 -0300

    retrofit e gps-tracking(inicio)

---
+++ retrofit-example/package/util/class/Obj.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: viniciusthiengo
+ * Date: 10/11/15
+ * Time: 10:29 PM
+ */
+
+class Obj {
+    public $name;
+    public $pathImg;
+    public $dataCriacao;
+
+
+    public function __construct( $name, $pathImg ){
+        $this->name = $name;
+        $this->pathImg = $pathImg;
+    }
+}
